8f7d1470c5bfc401583ff24b983d2bde6879066f
SanderTheDragon <sanderthedragon@zoho.com>
2016-09-01 10:51:50 +0200

    Added some directives, from my phone

--- src/Config.hpp
+++ src/Config.hpp
@@ -5,6 +5,18 @@
 #define VER_MINOR 1
 #define VER_BUILD 1
 
+#if defined(WIN)
+#define PATH_SEP "\\"
+#else
+#define PATH_SEP "/"
+#endif
+
+#define RES "res"
+#define RES_OBJECTS RES PATH_SEP "objects"
+#define RES_TEXTURES RES PATH_SEP "textures"
+#define RES_SOUNDS RES PATH_SEP "sounds"
+#define RES_LEVELS RES PATH_SEP "levels"
+
 #if !defined(WIN)
 #define NEWLINE "\n"
 #else
